a6e84f0787e37cfb9ee53de726fe2d0bfc36af0a
DieFleitas <diegofleitas98@outlook.es>
2022-02-02 20:04:13 -0300

    getting the source files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact />
+      </Switch>
 
-    </div>
+    </Router>
+    </>
   );
 }
 
